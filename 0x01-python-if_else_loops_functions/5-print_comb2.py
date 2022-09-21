06fb4dc7bfafad9f4a1c110e97a240584e249d8c
root <ayenifavour203@gmail.com>
2022-09-21 02:03:51 -0700

    Prints numbers

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for number in range(0, 99):
-    if number == 99:
+for number in range(0, 100 ):
+    if number == 100 :
         print("{}".format(number))
     else:
         print("{:02}".format(number), end=", ")
