e17c8e5708df017dc180dd68684cac655769c473
root <ayenifavour203@gmail.com>
2022-09-21 03:00:45 -0700

    Reverse the ASCII table

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-for i in range(ord(z), ord(a) - 1, -1):
-    if i % 2 == 0:
-        diff = 0
-    else:
-        diff = 32
-    print({}.format(chr(i - diff)), end=)
+i = 0
+for c in range(ord('z'), ord('a') - 1, -1):
+    print("{}".format(chr(c - i)), end="")
+    i = 32 if i == 0 else 0 )
