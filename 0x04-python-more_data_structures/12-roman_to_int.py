ce64c22f7a4bc4c07ebcb9f02aa8e7f42e6d5b3c
root <ayenifavour203@gmail.com>
2022-09-28 16:14:57 -0700

    Roman to Integer

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,45 +1,20 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    # Fail checks, none, not a string
-    if not roman_string:
-        return 0
-    if not isinstance(roman_string, str):
-        return 0
-    if not roman_string.isupper():
-        return 0
-    # Dictionary for roman numerals
-    r_dict = {
-        "I": 1,
-        "IV": 4,
-        "V": 5,
-        "IX": 9,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000
-    }
+    if type(roman_string) == str:
+        sum_v = 0
+        num = {I: 1, V: 5, X: 10, L: 50, C: 100, D: 500, M: 1000}
 
-    result = 0
-    temp = list(roman_string)
-    # Concat 4 and 9s
-    if len(temp) > 1:
-        idx = 0
-        for i in temp:
-            try:
-                if temp[idx] == I and temp[idx + 1] == V:
-                    temp[idx:idx + 2] = [.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            try:
-                if temp[idx] == I and temp[idx + 1] == X:
-                    temp[idx:idx + 2] = [.join(temp[idx:idx + 2])]
-            except IndexError:
-                pass
-            idx += 1
-    # Search in dict for correct numbers and add
-    for k, v in r_dict.items():
-        for index in temp:
-            if index == k:
-                result += v
-    return result
+        for i in range(len(roman_string)):
+
+            if i == len(roman_string) - 1:
+                sum_v += num[roman_string[i]]
+
+            elif num[roman_string[i + 1]] <= num[roman_string[i]]:
+                sum_v += num[roman_string[i]]
+
+            else:
+                sum_v -= num[roman_string[i]]
+
+        return (sum_v)
+    else:
+        return (0)
